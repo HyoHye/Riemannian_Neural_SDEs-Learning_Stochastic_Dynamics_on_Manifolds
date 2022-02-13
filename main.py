271b66afce95bd5d37cbe7f0ca72dbee2d44825d
Hyomin Kim <kim123hyomin@gmail.com>
2022-02-13 15:28:35 +0900

    [DataLoader] Update the main
    main.py

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-import numpy as np
-import torch
-from dataloader import CustomDataset
+import dataloader
 
-from torch.utils.data import DataLoader
+DATA_DIR = './data/'
 
-TRAIN_RATIO = 0.8
-VALIDATION_RATIO = 0.1
-TEST_RATIO = 0.1
-
-data = torch.tensor(np.load('without_noise.npy'))
-dataset = CustomDataset(data)
-
-train_len = int(len(dataset)*TRAIN_RATIO)
-validation_len = int(len(dataset)*VALIDATION_RATIO)
-
-train_dataset = dataset[:train_len]
-validation_dataset = dataset[train_len:train_len+validation_len]
-test_dataset = dataset[train_len+validation_len:]
-
-train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-validation_dataloader = DataLoader(validation_dataset, batch_size=128, shuffle=False)
-test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+train_loader, validation_loader, test_loader = dataloader.load_data(DATA_DIR+'NoisySphericalPendulum.npy', batch_size=1, nb_workers=0)
